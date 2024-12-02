32e7fea13f06b63590e321cb85a1442524a9bcaa
jswierczynski <kuba.swierczynski4@gmail.com>
2024-12-02 10:47:27 +0100

    lab8

--- lab8/lab_8_pthreads/CzytPis_Pthreads/czytelnia.c
+++ lab8/lab_8_pthreads/CzytPis_Pthreads/czytelnia.c
@@ -1,73 +1,62 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<unistd.h>
-#include<pthread.h>
-
-#include"czytelnia.h"
-
-/*** Implementacja procedur interfejsu ***/
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "czytelnia.h"
 
 int my_read_lock_lock(cz_t* cz_p) {
     pthread_mutex_lock(&cz_p->mutex);
-    printf("czytelnik próbuje wejść: l_c=%d, l_p=%d, waiting_writers=%d\n", cz_p->l_c, cz_p->l_p, cz_p->waiting_writers);
-    while (cz_p->l_p > 0 || cz_p->waiting_writers > 0) {
+    printf("czytelnik próbuje wejść: l_c=%d, l_p=%d\n", cz_p->l_c, cz_p->l_p);
+    while (cz_p->l_p > 0 || cz_p->waiting_writers) {
         pthread_cond_wait(&cz_p->czytelnicy_cond, &cz_p->mutex);
     }
     cz_p->l_c++;
-    printf("czytelnik wszedł: l_c=%d, l_p=%d, waiting_writers=%d\n", cz_p->l_c, cz_p->l_p, cz_p->waiting_writers);
+    printf("czytelnik wszedł: l_c=%d, l_p=%d\n", cz_p->l_c, cz_p->l_p);
     pthread_mutex_unlock(&cz_p->mutex);
     return 0;
 }
-
 
 int my_read_lock_unlock(cz_t* cz_p) {
     pthread_mutex_lock(&cz_p->mutex);
     cz_p->l_c--;
-    printf("czytelnik wychodzi: l_c=%d, l_p=%d, waiting_writers=%d\n", cz_p->l_c, cz_p->l_p, cz_p->waiting_writers);
-
-    if (cz_p->l_c == 0 && cz_p->waiting_writers > 0) {
-        // Jeśli nie ma już czytelników i są oczekujący pisarze, budzimy jednego pisarza
+    printf("czytelnik wychodzi: l_c=%d, l_p=%d\n", cz_p->l_c, cz_p->l_p);
+    if (cz_p->l_c == 0) {
         pthread_cond_signal(&cz_p->pisarze_cond);
     }
     pthread_mutex_unlock(&cz_p->mutex);
     return 0;
 }
-
 
 int my_write_lock_lock(cz_t* cz_p) {
     pthread_mutex_lock(&cz_p->mutex);
-    printf("pisarz próbuje wejść: l_c=%d, l_p=%d, waiting_writers=%d\n", cz_p->l_c, cz_p->l_p, cz_p->waiting_writers);
-
-    if (cz_p->l_p > 0 || cz_p->l_c > 0) {
-        cz_p->waiting_writers++; // Zwiększamy licznik oczekujących pisarzy
-        while (cz_p->l_p > 0 || cz_p->l_c > 0) {
-            pthread_cond_wait(&cz_p->pisarze_cond, &cz_p->mutex);
-        }
-        cz_p->waiting_writers--; // Zmniejszamy licznik po zakończeniu oczekiwania
+    printf("pisarz próbuje wejść: l_c=%d, l_p=%d\n", cz_p->l_c, cz_p->l_p);
+    cz_p->waiting_writers = 1;
+    while (cz_p->l_c > 0 || cz_p->l_p > 0) {
+        pthread_cond_wait(&cz_p->pisarze_cond, &cz_p->mutex);
     }
+    cz_p->waiting_writers = 0;
     cz_p->l_p++;
-    printf("pisarz wszedł: l_c=%d, l_p=%d, waiting_writers=%d\n", cz_p->l_c, cz_p->l_p, cz_p->waiting_writers);
+    printf("pisarz wszedł: l_c=%d, l_p=%d\n", cz_p->l_c, cz_p->l_p);
     pthread_mutex_unlock(&cz_p->mutex);
     return 0;
 }
-
 
 int my_write_lock_unlock(cz_t* cz_p) {
     pthread_mutex_lock(&cz_p->mutex);
-    cz_p->l_p--;
-    printf("pisarz wychodzi: l_c=%d, l_p=%d, waiting_writers=%d\n", cz_p->l_c, cz_p->l_p, cz_p->waiting_writers);
 
-    if (cz_p->waiting_writers > 0) {
-        // Jeśli są oczekujący pisarze, budzimy jednego z nich
-        pthread_cond_signal(&cz_p->pisarze_cond);
+    cz_p->l_p--;  // Pisarz wychodzi
+    printf("pisarz wychodzi: l_c=%d, l_p=%d\n", cz_p->l_c, cz_p->l_p);
+
+    // Wybudzaj wątki w zależności od sytuacji
+    if (cz_p->waiting_writers) {
+        pthread_cond_signal(&cz_p->pisarze_cond);  // Priorytet dla pisarzy
     } else {
-        // Jeśli nie ma oczekujących pisarzy, budzimy wszystkich czytelników
-        pthread_cond_broadcast(&cz_p->czytelnicy_cond);
+        pthread_cond_broadcast(&cz_p->czytelnicy_cond);  // Jeśli brak pisarzy, wybudzaj czytelników
     }
+
     pthread_mutex_unlock(&cz_p->mutex);
     return 0;
 }
-
 
 void inicjuj(cz_t* cz_p) {
     cz_p->l_p = 0;
@@ -81,19 +70,14 @@ void inicjuj(cz_t* cz_p) {
 
 void czytam(cz_t* cz_p) {
     pthread_mutex_lock(&cz_p->mutex);
-    printf("\t\t\t\t\tczytam:  l_c=%d, l_p=%d\n", cz_p->l_c, cz_p->l_p);
-
-
+    printf("\t\t\tczytam:  l_c=%d, l_p=%d\n", cz_p->l_c, cz_p->l_p);
     pthread_mutex_unlock(&cz_p->mutex);
     usleep(rand() % 3000000);
 }
 
 void pisze(cz_t* cz_p) {
     pthread_mutex_lock(&cz_p->mutex);
-    printf("\t\t\t\t\tpisze:   l_c=%d, l_p=%d\n", cz_p->l_c, cz_p->l_p);
-
-
-
+    printf("\t\t\tpisze:   l_c=%d, l_p=%d\n", cz_p->l_c, cz_p->l_p);
     pthread_mutex_unlock(&cz_p->mutex);
     usleep(rand() % 3000000);
 }
