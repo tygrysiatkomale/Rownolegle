608b13e4bf6cc2402503a44587d75eed8e821a8b
jswierczynski <kuba.swierczynski4@gmail.com>
2025-01-07 01:46:44 +0100

    lab6 od nowa

--- lab6/Histogram_test.java
+++ lab6/Histogram_test.java
@@ -1,47 +1,5 @@
-//import java.util.Scanner;
-//
-//class Histogram_test {
-//
-//	public static void main(String[] args) {
-//
-//		Scanner scanner = new Scanner(System.in);
-//
-//		System.out.println("Set image size: n (#rows), m(#columns)");
-//		int n = scanner.nextInt();
-//		int m = scanner.nextInt();
-//		Obraz obraz_1 = new Obraz(n, m);
-//
-//		// Obliczanie i drukowanie histogramu sekwencyjnie
-//		obraz_1.calculate_histogram();
-//		obraz_1.print_histogram();
-//
-//		// Tworzenie wątków dla każdego znaku
-//		CharacterThread[] watki = new CharacterThread[94];
-//
-//		for (int i = 0; i < 94; i++) {
-//			watki[i] = new CharacterThread(obraz_1.tab_symb[i], obraz_1);
-//			watki[i].setName("Wątek " + (i + 1));
-//			watki[i].start();
-//		}
-//
-//		// Oczekiwanie na zakończenie wszystkich wątków
-//		for (int i = 0; i < 94; i++) {
-//			try {
-//				watki[i].join();
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//
-//		// Porównanie histogramów
-//		obraz_1.compare_histograms();
-//
-//	}
-//
-//}
-
-
 import java.util.Scanner;
+
 
 class Histogram_test {
 
@@ -49,44 +7,28 @@ class Histogram_test {
 
 		Scanner scanner = new Scanner(System.in);
 
-		System.out.println("Set image size: n (#rows), m(#columns)");
+		System.out.println("Set image size: n (#rows), m(#kolumns)");
 		int n = scanner.nextInt();
 		int m = scanner.nextInt();
 		Obraz obraz_1 = new Obraz(n, m);
 
-		// Obliczanie i drukowanie histogramu sekwencyjnie
 		obraz_1.calculate_histogram();
 		obraz_1.print_histogram();
 
-		// Ustalanie liczby wątków i zakresów
-		System.out.println("Set number of threads:");
-		int num_threads = scanner.nextInt();
+		// System.out.println("Set number of threads");
+		// int num_threads = scanner.nextInt();
 
-		int zakres = 94 / num_threads;
-		Thread[] watki = new Thread[num_threads];
+		// Watek[] NewThr = new Watek[num_threads];
 
-		for (int i = 0; i < num_threads; i++) {
-			int startIndex = i * zakres;
-			int endIndex = (i == num_threads - 1) ? 94 : startIndex + zakres;
+		// for (int i = 0; i < num_threads; i++) {
+		//     (NewThr[i] = new Watek(...,obraz_1)).start();
+		// }
 
-			CharacterThread watekZakres = new CharacterThread(startIndex, endIndex, obraz_1);
-			watki[i] = new Thread(watekZakres);
-			watki[i].setName("Wątek " + (i + 1));
-			watki[i].start();
-		}
-
-
-		// Oczekiwanie na zakończenie wszystkich wątków
-		for (int i = 0; i < num_threads; i++) {
-			try {
-				watki[i].join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-
-		// Porównanie histogramów
-		obraz_1.compare_histograms();
+		// for (int i = 0; i < num_threads; i++) {
+		//     try {
+		// 	NewThr[i].join();
+		//     } catch (InterruptedException e) {}
+		// }
 
 	}
 
--- lab6/Obraz.java
+++ lab6/Obraz.java
@@ -1,7 +1,4 @@
-import java.util.Random;
-
-
-
+import  java.util.Random;
 
 
 class Obraz {
@@ -9,7 +6,7 @@ class Obraz {
 	private int size_n;
 	private int size_m;
 	private char[][] tab;
-	public char[] tab_symb;
+	private char[] tab_symb;
 	private int[] histogram;
 	private int[] hist_parallel;
 
@@ -22,15 +19,15 @@ class Obraz {
 
 		final Random random = new Random();
 
-		// Wypełnianie tablicy symbolami ASCII od 33 do 126
-		for(int k=0; k<94; k++) {
+		// for general case where symbols could be not just integers
+		for(int k=0;k<94;k++) {
 			tab_symb[k] = (char)(k+33); // substitute symbols
 		}
 
-		// Wypełnianie obrazu losowymi znakami
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<m; j++) {
-				tab[i][j] = tab_symb[random.nextInt(94)];  // ascii 33-126
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<m;j++) {
+				tab[i][j] = tab_symb[random.nextInt(94)];  // ascii 33-127
+				//tab[i][j] = (char)(random.nextInt(94)+33);  // ascii 33-127
 				System.out.print(tab[i][j]+" ");
 			}
 			System.out.print("\n");
@@ -38,103 +35,63 @@ class Obraz {
 		System.out.print("\n\n");
 
 		histogram = new int[94];
-		hist_parallel = new int[94]; // Tablica dla histogramu równoległego
 		clear_histogram();
-		clear_hist_parallel();
+
+		hist_parallel = new int[94];
+		clear_histogram_parallel();
 	}
 
-	public void clear_histogram() {
-		for(int i=0; i<94; i++) histogram[i]=0;
+	public void clear_histogram(){
+		for(int i=0;i<94;i++) histogram[i]=0;
 	}
 
-	public void clear_hist_parallel() {
-		for(int i=0; i<94; i++) hist_parallel[i]=0;
+	public void clear_histogram_parallel(){
+		for(int i=0;i<94;i++) hist_parallel[i]=0;
 	}
 
-	// Obliczanie histogramu sekwencyjnie
-	public void calculate_histogram() {
+	public void calculate_histogram(){
 
-		for(int i=0; i<size_n; i++) {
-			for(int j=0; j<size_m; j++) {
-				histogram[(int)tab[i][j]-33]++;
-			}
-		}
+		for(int i=0;i<size_n;i++) {
+			for(int j=0;j<size_m;j++) {
 
-	}
+				// optymalna wersja obliczania histogramu, wykorzystujÄca fakt, Ĺźe symbole w tablicy
+				// moĹźna przeksztaĹciÄ w indeksy tablicy histogramu
+				// histogram[(int)tab[i][j]-33]++;
 
-	// Obliczanie histogramu równolegle dla danego znaku
-	public void calculate_histogram_parallel(char znak) {
-		int count = 0;
-		for(int i=0; i<size_n; i++) {
-			for(int j=0; j<size_m; j++) {
-				if(tab[i][j] == znak) {
-					count++;
+				// wersja bardziej ogĂłlna dla tablicy symboli nie utoĹźsamianych z indeksami
+				// tylko dla tej wersji sensowne jest zrĂłwnoleglenie w dziedzinie zbioru znakĂłw ASCII
+				for(int k=0;k<94;k++) {
+					if(tab[i][j] == tab_symb[k]) histogram[k]++;
+					//if(tab[i][j] == (char)(k+33)) histogram[k]++;
 				}
+
 			}
 		}
-		int index = (int)znak - 33;
-		hist_parallel[index] = count;
+
 	}
 
-	// Drukowanie histogramu sekwencyjnego
-	public void print_histogram() {
+// uniwersalny wzorzec dla rĂłĹźnych wariantĂłw zrĂłwnoleglenia - moĹźna go modyfikowaÄ dla
+// rĂłĹźnych wersji dekompozycji albo stosowaÄ tak jak jest zapisane poniĹźej zmieniajÄc tylko
+// parametry wywoĹania w wÄtkach
+//
+//calculate_histogram_wzorzec(start_wiersz, end_wiersz, skok_wiersz,
+//                            start_kol, end_kol, skok_kol,
+//                            start_znak, end_znak, skok_znak){
+//
+//  for(int i=start_wiersz;i<end_wiersz;i+=skok_wiersz) {
+//     for(int j=start_kol;j<end_kol;j+=skok_kol) {
+//        for(int k=start_znak;k<end_znak;k+=skok_znak) {
+//           if(tab[i][j] == tab_symb[k]) histogram[k]++;
+//
 
-		for(int i=0; i<94; i++) {
+
+	public void print_histogram(){
+
+		for(int i=0;i<94;i++) {
 			System.out.print(tab_symb[i]+" "+histogram[i]+"\n");
+			//System.out.print((char)(i+33)+" "+histogram[i]+"\n");
 		}
 
 	}
 
-	// Drukowanie fragmentu histogramu dla danego wątku
-	public synchronized void print_histogram_parallel(char znak) {
-		int index = (int)znak - 33;
-		int count = hist_parallel[index];
-		System.out.print(Thread.currentThread().getName() + ": " + znak + " ");
-		for(int i=0; i<count; i++) {
-			System.out.print("=");
-		}
-		System.out.println();
-	}
-
-	// Porównanie histogramów sekwencyjnego i równoległego
-	public void compare_histograms() {
-		boolean equal = true;
-		for(int i=0; i<94; i++) {
-			if(histogram[i] != hist_parallel[i]) {
-				equal = false;
-				System.out.println("Różnica w histogramie dla znaku " + tab_symb[i]);
-			}
-		}
-		if(equal) {
-			System.out.println("Histogramy są identyczne.");
-		} else {
-			System.out.println("Histogramy nie są identyczne.");
-		}
-	}
-
-	public void calculate_histogram_parallel_range(int startIndex, int endIndex) {
-		for(int i = 0; i < size_n; i++) {
-			for(int j = 0; j < size_m; j++) {
-				char znak = tab[i][j];
-				int index = (int)znak - 33;
-				if(index >= startIndex && index < endIndex) {
-					synchronized(this) {
-						hist_parallel[index]++;
-					}
-				}
-			}
-		}
-	}
-
-	public synchronized void print_histogram_parallel_range(int startIndex, int endIndex) {
-		for(int k = startIndex; k < endIndex; k++) {
-			char znak = tab_symb[k];
-			int count = hist_parallel[k];
-			System.out.print(Thread.currentThread().getName() + ": " + znak + " ");
-			for(int i = 0; i < count; i++) {
-				System.out.print("=");
-			}
-			System.out.println();
-		}
-	}
 }
